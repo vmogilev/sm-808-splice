0539d9dff8d59ab9edcbe51755aca227057b1b65
vmogilev <oracle.ux@gmail.com>
2017-06-17 22:46:04 -0700

    Song and Pattern wrappers

--- drum.go
+++ drum.go
@@ -9,19 +9,21 @@ import (
 	"strings"
 )
 
-// Pattern - a pattern can be added to a song, each pattern has 8 beat slots
-// that can be filled with int values used as follows:
+// Pattern - a pattern can be added to a song, each pattern can have
+// any number of beats assigned as a map of step ID to int values measuring
+// velocity as follows:
 //
-//      0 - no beat - skip
-//      1 - output one bit at velocity 1
-//      2 - output one bit at velocity 2
-//      n - output one bit at velocity n ...
+//      step[1] = 0 - no beat - skip
+//      step[2] = 1 - one beat at velocity 1
+//      step[3] = 2 - one beat at velocity 2
+//      step[n] = n - one beat at velocity n
+//      ...
 //
 // File is an optinal sound file to play
 type Pattern struct {
 	Name  string
-	Beats []int  // beats in 8 step measure
-	File  string // Optional sound file to play
+	Beats map[int]int // Map of beats velocity for each step of a pattern
+	File  string      // Optional sound file to play
 }
 
 // Song is a collection of patterns under one name and tempo (measured in bpm)
@@ -31,31 +33,54 @@ type Song struct {
 	Tempo    int
 }
 
+const defaultBpm = 128
+const defaultSongName = "Four-on-the-floor"
+
 func main() {
 	var bpm int
 	var err error
 	for {
+		fmt.Printf("\tEnter Tempo (bpm):")
 		if bpm, err = parseTempo(os.Stdin); err == nil {
 			break
 		}
 		fmt.Printf("\t\t ** %s **\n", err.Error())
 	}
-	fmt.Println(bpm)
+	fmt.Printf("Using %d BPM ...\n", bpm)
+}
+
+// NewSong - creates a new song with a default title if none provided
+func NewSong(t string, tempo int) *Song {
+	title := t
+	if title == "" {
+		title = defaultSongName
+	}
+	return &Song{
+		Name:  title,
+		Tempo: tempo,
+	}
+}
+
+// AddPattern - adds a specific beat pattern to existing song
+func (s *Song) AddPattern(name string, beats map[int]int) {
+	pat := Pattern{
+		Name:  name,
+		Beats: beats,
+	}
+	s.Patterns = append(s.Patterns, pat)
 }
 
 func parseTempo(r io.Reader) (int, error) {
-	var bpm = 128
+	var bpm int
 	var err error
 	var bpmStr string
 
 	reader := bufio.NewReader(r)
-	fmt.Printf("\tEnter Tempo (bpm):")
 	bpmStr, _ = reader.ReadString('\n')
 	bpmStr = strings.TrimSpace(bpmStr)
 
 	if len(bpmStr) == 0 {
-		fmt.Printf("\t\tusing default at %d BPM ...", bpm)
-		return bpm, nil
+		return defaultBpm, nil
 	}
 
 	bpm, err = strconv.Atoi(bpmStr)
--- drum_test.go
+++ drum_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -42,10 +43,35 @@ var testTempoCases = []struct {
 	},
 }
 
+var testNewSongCases = []struct {
+	title string
+	tempo int
+	song  *Song
+}{
+	{
+		"",
+		60,
+		&Song{Name: defaultSongName, Tempo: 60},
+	},
+	{
+		"November Rain",
+		128,
+		&Song{Name: "November Rain", Tempo: 128},
+	},
+}
+
 func TestTempo(t *testing.T) {
 	for _, test := range testTempoCases {
 		if out, ok := parseTempo(bytes.NewBufferString(test.in)); out != test.out || ok != test.ok {
 			t.Fatalf("In: %s Want: %d | %s\nGot: %d | %s\n", test.in, test.out, test.ok, out, ok)
+		}
+	}
+}
+
+func TestSong(t *testing.T) {
+	for _, test := range testNewSongCases {
+		if song := NewSong(test.title, test.tempo); !reflect.DeepEqual(song, test.song) {
+			t.Fatalf("Title: %s | Tempo: %d\nGot: %v | Want: %v", test.title, test.tempo, song, test.song)
 		}
 	}
 }
